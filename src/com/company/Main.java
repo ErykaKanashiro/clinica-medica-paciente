01dd81984eb7b4d5b3cf72291b004635e02b659d
Eryka Kanashiro <erykakanashiro@gmail.com>
2021-11-24 10:22:45 -0300

    Introdução de validação CPF e cálculo de peso ideal.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,14 @@
 package com.company;
+import java.util.Arrays;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String cpf = "399.601.638-73";
-        String[] partes = cpf.split(".",4);
-        String cpf2 = obterCFPOfuscado(partes);
 
-
-        System.out.println(cpf2.toString());
     }
 
-    private static String obterCFPOfuscado(String[] cpf) {
-
-        String CPF = "";
-        String partes[] = CPF.split(".",4);
-        String partes1[] = CPF.split(".",8);
-        String parte2[] = CPF.split("-",12);
-
-        return CPF;
-    }
 }
--- src/com/company/Paciente.java
+++ src/com/company/Paciente.java
@@ -1,11 +1,12 @@
 package com.company;
-
+import java.util.Arrays;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 import static java.time.LocalTime.parse;
 
-public class Paciente<imc> {
+public class Paciente {
 
     private String nome;
     private String sobrenome;
@@ -30,7 +31,7 @@ public class Paciente<imc> {
         imc = 0.0;
     }
 
-    public Paciente(String nome, String sobrenome, char sexo, LocalDate nascimento, short altura, double peso, String cpf) {
+    public Paciente(String nome, String sobrenome, char sexo, LocalDate nascimento, double altura, double peso, String cpf) {
         setNome(nome);
         setSobrenome(sobrenome);
         this.sexo = sexo;
@@ -41,7 +42,7 @@ public class Paciente<imc> {
         imc = calcularIMC();
     }
 
-    public Paciente(String nome, String sobrenome, char sexo, short altura, double peso, String cpf) {
+    public Paciente(String nome, String sobrenome, char sexo, double altura, double peso, String cpf) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.sexo = sexo;
@@ -51,7 +52,7 @@ public class Paciente<imc> {
         imc = calcularIMC();
     }
 
-    public Paciente(String nome, String sobrenome, char sexo, LocalDate nascimento, short altura, double peso) {
+    public Paciente(String nome, String sobrenome, char sexo, LocalDate nascimento, double altura, double peso) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.sexo = sexo;
@@ -73,7 +74,7 @@ public class Paciente<imc> {
 
     public void setNome(String nome) {
         if (nome != "" && nome != null)
-        this.nome = nome;
+            this.nome = nome;
     }
 
     public String getSobrenome() {
@@ -81,9 +82,9 @@ public class Paciente<imc> {
     }
 
     public void setSobrenome(String sobrenome) {
-        if (sobrenome != "" && sobrenome != null)
+        if (!Objects.equals(sobrenome, "") && sobrenome != null)
 
-        this.sobrenome = sobrenome;
+            this.sobrenome = sobrenome;
     }
 
     public char getSexo() {
@@ -94,12 +95,14 @@ public class Paciente<imc> {
         this.sexo = sexo;
     }
 
-    public LocalDate getNascimento() {
-        return nascimento;
+    public String getNascimento() {
+
+        return nascimento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
 
     public void setNascimento(LocalDate nascimento) {
         this.nascimento = nascimento;
+        this.idade = calcularIdade();
     }
 
     public byte getIdade() {
@@ -110,12 +113,8 @@ public class Paciente<imc> {
         return altura;
     }
 
-    public void setAltura(short altura) {
-
-        if (altura > 0)
-        this.altura = altura;
-        imc = calcularIMC();
-
+    public void setAltura(double altura) {
+            this.altura = altura;
     }
 
     public double getPeso() {
@@ -124,84 +123,140 @@ public class Paciente<imc> {
 
     public void setPeso(double peso) {
 
-        if (peso >= 0.0 )
-        this.peso = peso;
+        if (peso >= 0.0)
+            this.peso = peso;
         imc = calcularIMC();
 
     }
 
     public String getCpf() {
-        return cpf;
+         return cpf.replaceFirst("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "***.$2.***-**");
     }
 
     public void setCpf(String cpf) {
-        this.cpf = cpf;
+
+        if (cpf.length() == 11 && validarCPF(cpf)) {
+            this.cpf = cpf;
+        }
+    }
+
+    public double getPesoIdeal() {
+
+        return pesoIdeal = obterPesoIdeal();
+
     }
 
     public double getImc() {
         return imc;
     }
 
-    public double obterPesoIdeal(char sexo, short altura) {
+    public double obterPesoIdeal() {
+
+        //System.out.println(altura);
+        //pesoIdeal = 0.0;
 
         if (sexo == 'M') {
             pesoIdeal = (72.7 * altura) - 58;
 
-        } else if (sexo == 'M') {
+        } else {
             pesoIdeal = (62.1 * altura) - 44.7;
 
-        } else {
-            System.out.println("Sexo inválido");
-
-        }
+        }          //System.out.println(pesoIdeal);
         return pesoIdeal;
     } // fim metodo ObterPesoIdeal
 
-    public void obterCPFOfuscado(){
+    static String limparCPF(String cpf) {
+        String cpfLimpo = "";
+        cpfLimpo = cpf.replaceAll("\\D+", "");
+        return cpfLimpo;
+    }
 
+    static String formatarCPF(String cpf) {
 
-        getCpf();
-        String partes[] = getCpf().split(".",4);
-        String partes1[] = getCpf().split(".",8);
-        String parte2[] = getCpf().split("-",12);
+        String cpfFormatado = "";
 
-        return;
+        cpfFormatado = cpf.replaceFirst("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+
+        return cpfFormatado;
+    }
+
+    private String obterCPFOfuscado() {
+
+        String cpfFormatado = "";
+
+        cpfFormatado = cpf.replaceFirst("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "***.$2.***-**");
+
+        return cpfFormatado;
 
     }
 
+    private int[] calcularDigitoVerificador(char[] cpf) {
+
+        int[] digitosVerificadores = new int[2];
+        int[] digitosCPF = new int[9];
+
+        int v1 = -1;
+        int v2 = -1;
+
+
+        // Preenche o vetor digitos CPF de forma inversa
+        // o menor índice deve conter o dígito mais à direita do CPF
+        for (int i = 8; i >= 0; i--)
+            digitosCPF[(digitosCPF.length - 1) - i] = Character.getNumericValue(cpf[i]);
+
+
+        for (int i = 0; i < digitosCPF.length; i++) {
+
+            v1 = v1 + digitosCPF[i] * (9 - (i % 10));
+            v2 = v2 + digitosCPF[i] * (9 - ((i + 1) % 10));
+
+        }
+
+
+        v1 = (v1 % 11) % 10;
+        v2 = v2 + v1 * 9;
+        v2 = (v2 % 11) % 10;
+
+        digitosVerificadores[0] = v2;
+        digitosVerificadores[1] = v1;
+
+        return digitosVerificadores;
+
+    } // Fim do método calcular dígitos verificadores
+
     public void obterSituacaoIMC() {
 
-        imc = peso /(altura*altura);
+        imc = peso / (altura * altura);
 
-        if ( altura <= 0 || altura >= 3 || peso <= 0){
-            System.out.println ("Entrada inválida");
+        if (altura <= 0 || altura >= 3 || peso <= 0) {
+            System.out.println("Entrada inválida");
         } else {
 
             if (imc < 17) {
 
                 System.out.println("Muito abaixo do peso.");
 
-            } else if (imc > 17 && imc < 18.49){
+            } else if (imc > 17 && imc < 18.49) {
 
                 System.out.println("Abaixo do peso.");
 
-            } else if (imc > 18.50 && imc < 24.99){
+            } else if (imc > 18.50 && imc < 24.99) {
 
                 System.out.println("Peso normal.");
 
-            } else if (imc > 25 && imc < 29.99){
+            } else if (imc > 25 && imc < 29.99) {
 
                 System.out.println("Acima do peso.");
 
-            } else if (imc > 30 && imc < 34.99){
+            } else if (imc > 30 && imc < 34.99) {
 
                 System.out.println("Obesidade I.");
 
-            } else if (imc > 35 && imc < 39.99){
+            } else if (imc > 35 && imc < 39.99) {
 
                 System.out.println("Obesidade II.");
 
-            } else if (imc >= 40){
+            } else if (imc >= 40) {
 
                 System.out.println("Obesidade III.");
 
@@ -215,27 +270,23 @@ public class Paciente<imc> {
 
     } // fim metodo calcularIMC
 
-    private void calcularIdade(LocalDate data, LocalDate nascimento){
+    private byte calcularIdade() {
 
-        LocalDate localDate = LocalDate.parse("12/06/1989", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-      //  idade =
+        int anoAtual = LocalDate.now().getYear();
+        int anoNascimento = nascimento.getYear();
+
+                return (byte)(anoAtual - anoNascimento);
     }
 
     private boolean validarCPF(String cpf) {
 
-        if (cpf == "22222222222" || cpf == "3333333333" || cpf == "44444444444" || cpf == "55555555555"
-                || cpf == "66666666666" || cpf == "7777777777" || cpf == "88888888888" || cpf == "99999999999" || cpf.length() != 11)
+        char[] digitosCPF = cpf.toCharArray();
 
-        return (false);
+        int v1 = Character.getNumericValue(cpf.charAt(9));
+        int v2 = Character.getNumericValue(cpf.charAt(10));
 
+        int[] digitosVerificados = calcularDigitoVerificador(digitosCPF);
 
-        cpf = cpf.replaceAll("-", "");
-        cpf = cpf.replaceAll("_", "");
-        cpf = cpf.replaceAll("/", "");
-        cpf = cpf.replaceAll("[^0-9]+", "");
-
-        return (true);
+        return (digitosVerificados[0] == v1) && (digitosVerificados[1] == v2);
     }
-
- // FIM DO METODO MAIN
 }// fim class Paciente
